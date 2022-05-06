3b76945f4664f09e6690ed5fde6529c90fa37f61
Antoine El Choueiri <anthonychoueiri@hotmail.com>
2022-05-06 10:26:35 +0300

    refactor: move routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Departments from "./components/Departments";
+import GroupList from "./components/GroupList";
+import Group from "./components/Group";
+import Employee from "./components/Employee";
+import Search from "./components/Search";
 import "./sass/main.scss";
 
-type AppProps = {
-  children: React.ReactNode;
-};
-
-const App = ({ children }: AppProps): JSX.Element => {
+const App = (): JSX.Element => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="App">{children}</div>
-    </>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Employee />} />
+          <Route path="/departments" element={<Departments />} />
+          <Route path="/group/:groupType" element={<GroupList />} />
+          <Route path="/group/:groupType/:group" element={<Group />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
-import Home from "./components/Home";
-import Departments from "./components/Departments";
-import GroupList from "./components/GroupList";
-import Group from "./components/Group";
-import Employee from "./components/Employee";
-import Search from "./components/Search";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<Employee />} />
-          <Route path="/departments" element={<Departments />} />
-          <Route path="/group/:groupType" element={<GroupList />} />
-          <Route path="/group/:groupType/:group" element={<Group />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </App>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
